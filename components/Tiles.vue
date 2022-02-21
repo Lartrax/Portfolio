8fb65805244b47de661d112916d6ddfe9787a9a9
Lars Erik Nordbø <lartrax909@gmail.com>
2022-02-21 22:48:07 +0100

    added tiles to download and prepared json

---
+++ components/Tiles.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script lang="ts">
+import Vue from 'vue';
+import tileData from '../static/tiles.json';
+export default Vue.extend({
+  name: "tiles",
+  data() {
+    return {
+      tile: tileData,
+    };
+  },
+});
+</script>
+
+<style></style>
--- pages/Downloads.vue
+++ pages/Downloads.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>Downloads</div>
+  <div>
+    <h1 class="text-3xl">Downloads</h1>
+    <Tiles />
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
